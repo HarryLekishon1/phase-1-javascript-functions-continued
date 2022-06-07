566d1ee34768fbe5cd476c6a1a3f23b4b298ccd0
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-07 19:05:37 +0300

    function and wrapped functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(game = "roller-skate"){
+    return `This Saturday, I want to ${game}!`;
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(gift){
+    return function(special = "special"){
+
+        return `You are ${gift}${special}${gift}!`
+    }
+}
